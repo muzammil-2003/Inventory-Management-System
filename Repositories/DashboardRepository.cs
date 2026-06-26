9c022787cd7cdc9000d1ac09d8676447784c21ca
Muzammil Ahmed Mangrio <ahmedmuzammil144@gmail.com>
2026-06-26 19:50:12 +0500

    Add DashboardRepository for dashboard statistics
    Added DashboardRepository.cs and updated project file. The new repository provides methods to retrieve total products, categories, suppliers, total stock, and inventory value from the database using SQL queries.

---
+++ Repositories/DashboardRepository.cs
@@ -0,0 +1,60 @@
+﻿using InventoryManagementSystem.Database;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace InventoryManagementSystem.Repositories
+{
+    public class DashboardRepository
+    {
+        public int GetTotalProducts()
+        {
+            using (SqlConnection conn = new SqlConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Products";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return Convert.ToInt32(cmd.ExecuteScalar());
+            }
+        }
+        public int GetTotalCategories()
+        {
+            using (SqlConnection conn = new SqlConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Categories";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+        public int GetTotalSuppliers()
+        {
+            using (SqlConnection conn = new SqlConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Suppliers";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+        public int GetTotalStock()
+        {
+            using (SqlConnection conn = new SqlConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string query = "SELECT ISNULL(SUM(Quantity), 0) FROM Products";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+        public decimal GetInventoryValue()
+        {
+            using (SqlConnection conn = new SqlConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string query = "SELECT ISNULL(SUM(Price * Quantity),0) FROM Products";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                return Convert.ToInt32(cmd.ExecuteScalar());
+            }
+        }
+    }
+}
